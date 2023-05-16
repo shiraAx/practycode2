5c0f2d4645b7104b2479b9a351776e333f7cf462
shiraAx <shira89516@gmail.com>
2023-05-16 16:51:30 +0300

    last

--- nodeExpressApp/app.js
+++ nodeExpressApp/app.js
@@ -23,14 +23,14 @@ const require = createRequire(import.meta.url);
 const app = express();
 const port = 3000;
 
-const sdk = require("api")("@render-api/v1.0#dnrc1ulf088q9j");
 
-sdk.auth("rnd_IUAofOFZ8o28tkrrVF4wzFeknbyC");
-sdk
-  .getServices({ limit: "20" })
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+
+sdk.auth('rnd_IUAofOFZ8o28tkrrVF4wzFeknbyC');
+sdk.getServices({limit: '20'})
   .then(({ data }) => console.log(data))
-  .catch((err) => console.error(err));
-
+  .catch(err => console.error(err));
+  
 app.get("/", (req, res) => {
   res.send("hello world");
 });
