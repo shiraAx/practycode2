d7597bb13cffac6a77f792f5cdd7bad8664a7d4a
shiraAx <shira89516@gmail.com>
2023-05-16 15:53:47 +0300

    last

--- nodeExpressApp/app.js
+++ nodeExpressApp/app.js
@@ -15,6 +15,13 @@
 //     );
 //   else console.log("Error occurred, server can't start", error);
 // });
+import express from 'express'
+// import require from 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const app=express()
+const port=3000;
 
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
